fa07fecbc381f4fa6ad4ff4a99e344e041eb71de
pollycoder <pollyjoe2003@gmail.com>
2022-09-15 14:52:57 +0800

    modified readme

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # web_crawler_learning
+## Device UA: 
+Mozilla/5.0 (X11; Ubuntu; Linux x86_64; rv:104.0) Gecko/20100101 Firefox/104.0
--- basic.py
+++ basic.py
@@ -1,17 +1,24 @@
-# This is a sample Python script.
+# This is a sample of learning web crawling
+# Adapted from a construction on C-language Chinese Net
+# URL: http://c.biancheng.net/python_spider/the-first-spider.html
 
-# Press Shift+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
-import urllib.request as ul
+## Part 1: Get HTML info of one page
 from urllib import request
-from fake_useragent import UserAgent
-url = 'http://httpbin.org/get'
-headers = {
+# Open the web
+response = request.urlopen('http://httpbin.org/')
+print(response)
+# Get the HTML code of the web
+html = response.read().decode()
+print(html)
+
+## Part 2: User Agent
+# Get UA info
+response1 = request.urlopen('http://httpbin.org/get')
+html1 = response1.read().decode()
+print(html1)
+# Reconstruct request headers, pretending to be the browser
+url = 'http://httpbin.ogr/get'
+headers={
     'User-Agent': 'Mozilla/5.0 (X11; Ubuntu; Linux x86_64; rv:104.0) Gecko/20100101 Firefox/104.0'
 }
-req = request.Request(url=url, headers=headers)
-res = request.urlopen(req)
-html = res.read().decode('utf-8')
-print(html)
-ua = UserAgent()
-print(ua.firefox)
+
